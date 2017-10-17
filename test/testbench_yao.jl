154dbfbb46ca248b748e65403350c0d3f1deec21
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2017-10-17 07:31:37 +0200

    Adapted to new API

--- test/testbench_yao.jl
+++ test/testbench_yao.jl
@@ -12,28 +12,29 @@ opt    = Genoud.Options(f_tol = 1e-06, max_generations = 50, pmix = .5, print_le
 solver = Ipopt.IpoptSolver(print_level=0)
 srand(1)
 
-p, j, i = 1,1,1
+p, i, j = 1,1,1
 
-#for p = 1:4
-  mgens = parms[p][1]
-  spop  = parms[p][2]
-#  for i in 1:length(funs)
-    f      = funs[i]
-    fit    = deepcopy(f[:f])
-    bounds = copy(f[:domain])
-    domain = Genoud.Domain(bounds)
-    x0     = bounds[:,1] + rand(size(bounds, 1)).*(bounds[:,2] - bounds[:1])
-    
-    if f[:differentiable]
-      d = OnceDifferentiable(fit, x0)
-    else
-      d = NonDifferentiable(fit, x0)
+for p = 1:4
+    mgens = parms[p][1]
+    spop  = parms[p][2]
+    for i in 1:length(funs)
+        f      = funs[i]
+        fit    = deepcopy(f[:f])
+        bounds = copy(f[:domain])
+        domain = Genoud.Domain(bounds)
+        x0     = bounds[:,1] + rand(size(bounds, 1)).*(bounds[:,2] - bounds[:1])
+        
+        if f[:differentiable]
+            d = OnceDifferentiable(fit, x0)
+        else
+            d = NonDifferentiable(fit, x0)
+        end
+        for j in 1:nruns
+            print_with_color(:cyan, "Solving problem ", i, " out of 23 -> ")
+            print_with_color(:cyan, f[:fname], "\n")
+            times[i, j, p] = @elapsed out = Genoud.genoud(d, spop, domain, solver = solver, opt = opt)
+            fstar[i, j, p] = Genoud.bestfitns(out)
+            push!(solus, Genoud.bestindiv(out))
+        end
     end
-  
-    print_with_color(:cyan, "Solving problem ", i, " out of 23 -> ")
-    print_with_color(:cyan, f[:fname], "\n")
-    times[i, j, p] = @elapsed out = Genoud.genoud(d, spop, domain, solver = solver, opt = opt)
-    fstar[i, j, p] = Genoud.bestfitns(out)
-    push!(solus, Genoud.bestindiv(out))
-  #end
-#end
+end
